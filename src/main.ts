78fff15f27a490e8e3b4f0f4887d12dbf05f70ff
张一凡 <yifan.zhang>
2026-08-25 14:58:57 +0800

    feat: 上报小程序真实访问数据

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import store from "./stores";
 import "uno.css";
 import "@/styles/common.scss";
+import { setupVisitTracking } from "@/utils/visit-tracker";
 
 export function createApp() {
   const app = createSSRApp(App);
   app.use(store);
+  setupVisitTracking();
   return { app };
 }
